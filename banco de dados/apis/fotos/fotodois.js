81219ca4834db266a8b5424d085ebf1b09b8d04f
Your Name <you@example.com>
2022-12-10 21:10:31 +0000

    first commit

---
+++ banco de dados/apis/fotos/fotodois.js
@@ -0,0 +1,13 @@
+[
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"},
+{"result": "https://telegra.ph/file/72e775cef74f01d0cf0b7.jpg"}
+]
---
+++ banco de dados/menus/help.js
@@ -0,0 +1,30 @@
+const help = (emoji, prefix, tempo, pushname) => {
+ return `
+ A______A
+|・  ㅅ ・   |
+|っ   　       ｃ|
+|　        ❣ 　   |
+|　                  |
+|　                  　　|
+|　                  　　|
+|　   　　               |
+|　   　　               |　 
+|　   　　               |
+|　   　　               |
+|　   　　               |　 
+U￣￣￣U
+┃
+┃ANA BOT MD V1 CRIADO
+┃criador: BLUE EXORCIST MODZ 
+┃bot: ANA MD BOT
+┃créditos ao BLUE EXORCIST MODZ por ter feito o bot
+┣━━━───MENUS
+┃╭ ✧͢⃟ᤢ🌸-⋟ ${prefix}menu
+┃╰━━━─── • ──━━━━
+┃╭ ✧͢⃟ᤢ🌸-⋟ ${prefix}menu2
+┃╰━━━─── • ──━━━━
+┃╭ ✧͢⃟ᤢ🌸-⋟ ${prefix}help
+┃╰━━━─── • ──━━━━
+┗━━━━━━━━━━━━━━`
+}
+exports.help = help
